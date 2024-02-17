09eebf39e7493be80361e95b9ff88b5a28bc5caf
mohacelhosen <mohacel.hosen22@gmail.com>
2024-02-17 14:12:32 +0600

    Double-Checked Locking

--- src/singleton/SingletonPrinter.java
+++ src/singleton/SingletonPrinter.java
@@ -3,15 +3,94 @@ package singleton;
 public class SingletonPrinter {
 
     private static SingletonPrinter printer;
-    private SingletonPrinter(){
-        //no body
+
+    // Private constructor to prevent direct instantiation
+    private SingletonPrinter() {
+        //No body
     }
 
-    public static SingletonPrinter getInstance(){
-       if (printer==null){
-           printer = new SingletonPrinter();
-       }
-       return printer;
+    //primary level singleton easily multi thread can break singleton
+//    public static  SingletonPrinter getInstance() {
+//        if (printer == null) {
+//            printer = new SingletonPrinter();
+//        }
+//        return printer;
+//    }
+
+
+//primary level singleton easily multi thread can break singleton
+//    public static SingletonPrinter getInstance() {
+//            if (printer == null) {
+//                try {
+//                    Thread.sleep(10000);//introduces a race condition that can lead to the creation of multiple instances in a multi-threaded environment
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                printer = new SingletonPrinter();
+//            }
+//
+//        return printer;
+//    }
+
+
+
+    // singleton in multi thread environment but performance issue
+//    public static synchronized SingletonPrinter getInstance() {
+//        if (printer == null) {
+//            try {
+//                Thread.sleep(10000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            printer = new SingletonPrinter();
+//        }
+//        return printer;
+//    }
+
+
+    // class level locking
+//    public static SingletonPrinter getInstance() {
+//        System.out.println("Outside😆 syn block:: "+Thread.currentThread().getName());
+//        // "this" keyword can't be used in static method
+//        synchronized (SingletonPrinter.class) { //SingletonPrinter.class here ".class" is a static variable added by compiler. by this "SingletonPrinter.class" we know everything about "SingletonPrinter"
+//            System.out.println("Inside🙂 syn block:: "+Thread.currentThread().getName());
+//            if (printer == null) {
+//                try {
+//                    Thread.sleep(10000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                printer = new SingletonPrinter();
+//            }
+//        }
+//        return printer;
+//    }
+
+
+
+    //Double-Checked Locking Singleton Pattern with Lazy Initialization.
+    public static SingletonPrinter getInstance() {
+        System.out.println("Outside😆 syn block:: "+Thread.currentThread().getName());
+        // First check outside the synchronized block for performance
+        if (printer == null) {
+            // Synchronize only when the instance is not yet created
+            synchronized (SingletonPrinter.class) {
+                System.out.println("Inside🙂 syn block:: "+Thread.currentThread().getName());
+                // Double-check inside the synchronized block
+                if (printer == null) {
+                    System.out.println("🎁"+Thread.currentThread().getName());
+                    try {
+                        // Simulate time-consuming initialization
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // Create the singleton instance
+                    printer = new SingletonPrinter();
+                }
+            }
+        }
+        return printer;
     }
 
     public void printSomething() {
--- src/singleton/SingletonTest.java
+++ src/singleton/SingletonTest.java
@@ -4,8 +4,28 @@ import java.lang.reflect.Constructor;
 
 public class SingletonTest {
     public static void main(String[] args) {
+        // Create multiple threads to access singleton concurrently
+        Thread thread1 = new Thread(() -> accessSingleton());
+        Thread thread2 = new Thread(() -> accessSingleton());
+        Thread thread3 = new Thread(() -> accessSingleton());
+        Thread thread4 = new Thread(() -> accessSingleton());
+
+        thread1.start();
+        thread2.start();
+        try{
+            Thread.sleep(10000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        thread3.start();
+        thread4.start();
+
         SingletonPrinter p1 = SingletonPrinter.getInstance();
         SingletonPrinter p2 = SingletonPrinter.getInstance();
+
+        System.out.println("Thread " + Thread.currentThread().getId() + ": p1 hashCode= " + p1.hashCode());
+        System.out.println("Thread " + Thread.currentThread().getId() + ": p2 hashCode= " + p2.hashCode());
+
 
         System.out.println("p1= " + p1.hashCode() + "\n" + "p2= " + p2.hashCode());
 
@@ -17,12 +37,19 @@ public class SingletonTest {
             Constructor<SingletonPrinter> printerConstructor = SingletonPrinter.class.getDeclaredConstructor();
             printerConstructor.setAccessible(true);
             SingletonPrinter newPrinter = printerConstructor.newInstance();
-            System.out.println("p3= "+newPrinter.hashCode());
+            System.out.println("p3= " + newPrinter.hashCode());
+
 
         } catch (NoSuchMethodException ex) {
             ex.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    private static void accessSingleton() {
+        SingletonPrinter p4 = SingletonPrinter.getInstance();
+        System.out.println("Thread " + Thread.currentThread().getId() + ": p4 hashCode= " + p4.hashCode());
     }
 }
